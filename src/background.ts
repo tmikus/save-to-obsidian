07f4b2c9b25c41d66d9eb5911d8d912d1549bb3d
Tomasz Mikus <mikus.tomasz@gmail.com>
2024-05-29 16:51:09 +0100

    Add the `style.css` file.

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,4 @@
-import {SHOW_POPUP} from "./actions";
+import { SHOW_POPUP } from "./actions";
 
 const MENU_ID = "save-to-obsidian";
 
@@ -7,11 +7,11 @@ chrome.contextMenus.create({
   contexts: ["all"],
   id: MENU_ID,
   title: "Save to Obsidian",
-})
+});
 
 chrome.contextMenus.onClicked.addListener(async (info, tab) => {
-  if (!tab || !tab.id) return
+  if (!tab || !tab.id) return;
   await chrome.tabs.sendMessage(tab.id, {
-    action: SHOW_POPUP
-  })
-})
+    action: SHOW_POPUP,
+  });
+});
--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,16 +1,18 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
-import {ObsidianDialog} from "./obsidian_dialog";
+import { createRoot } from "react-dom/client";
+import { ObsidianDialog } from "./obsidian_dialog";
 
 const containerNode = document.createElement("div");
 document.body.appendChild(containerNode);
-const shadowRoot = containerNode.attachShadow({ mode: 'open' });
+const shadowRoot = containerNode.attachShadow({ mode: "open" });
 const rootNode = document.createElement("div");
-shadowRoot.appendChild(rootNode)
+shadowRoot.appendChild(rootNode);
 const root = createRoot(rootNode, {});
+const styleURL = chrome.runtime.getURL("style.css");
 
 root.render(
-    <React.StrictMode>
-        <ObsidianDialog />
-    </React.StrictMode>
+  <React.StrictMode>
+    <link rel="stylesheet" href={styleURL} />
+    <ObsidianDialog />
+  </React.StrictMode>
 );
